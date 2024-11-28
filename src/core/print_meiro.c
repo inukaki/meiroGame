dedde5b23c6724deba232f74ce57d0f6d699b36d
inukaki <inukaki8@gmail.com>
2024-11-28 16:15:55 +0900

    スタートとゴールもドットに変更

--- src/core/print_meiro.c
+++ src/core/print_meiro.c
@@ -7,8 +7,8 @@ void print_meiro(Meiro *meiro) {
             for (int j = 0; j < meiro->width; j++) {
                 if (meiro->cells[i][j].state == 0) printf("..");
                 else if (meiro->cells[i][j].state == 1) printf("##");
-                else if (meiro->cells[i][j].state == 2) printf("SS");
-                else if (meiro->cells[i][j].state == 3) printf("GG");
+                else if (meiro->cells[i][j].state == 2) printf("..");
+                else if (meiro->cells[i][j].state == 3) printf("..");
             }
             printf("\n");
         }
